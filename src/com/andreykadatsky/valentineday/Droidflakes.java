91e9ddcfccb33b81b8fb1c5b237bab4a1d179357
evgentset <tset.evgen@gamail.com>
2015-02-13 18:40:57 +0200

    droid

--- src/com/andreykadatsky/valentineday/Droidflakes.java
+++ src/com/andreykadatsky/valentineday/Droidflakes.java
@@ -19,9 +19,6 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 
 public class Droidflakes extends Activity {
@@ -37,40 +34,9 @@ public class Droidflakes extends Activity {
         container.addView(flakeView);
         getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLACK));
 
-        /*
-        Button more = (Button) findViewById(R.id.more);
-        more.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                flakeView.addFlakes(flakeView.getNumFlakes());
-            }
-        });
-        Button less = (Button) findViewById(R.id.less);
-        less.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                flakeView.subtractFlakes(flakeView.getNumFlakes() / 2);
-            }
-        });
-        */
-        HoneycombHelper.setup(this);
-
-//        flakeView.addFlakes(flakeView.getNumFlakes());
-
     }
 
-    private static final class HoneycombHelper {
-        static void setup(final Droidflakes activity) {
-            CheckBox accelerated = (CheckBox) activity.findViewById(R.id.accelerated);
-            accelerated.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    activity.flakeView.setLayerType(
-                            isChecked ? View.LAYER_TYPE_NONE : View.LAYER_TYPE_SOFTWARE, null);
-                }
-            });
-        }
-    }
+
 
     @Override
     protected void onPause() {
